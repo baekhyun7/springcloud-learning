6daca9602c4461408beb061c688f6285605e217f
zh <1334460325@qq.com>
2023-01-12 23:03:46 +0800

    init

---
+++ springcloud-consumer/src/main/java/com/zh/SpringcloudConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConsumerApplication.class, args);
+    }
+
+}
